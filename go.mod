fe5b907e3fb6781fdb8cd640cf707e9ba19bb9b5
Wagner <mobile.costa@gmail.com>
2023-04-21 22:07:35 -0300

    Desenvolvimento

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
-module github.com/mobilecosta/fclx/chatservice
+module github.com/mobilecosta/chatservice
 
 go 1.20
+
+require (
+	github.com/google/uuid v1.3.0
+)
--- internal/domain/entity/message.go
+++ internal/domain/entity/message.go
@@ -1,1 +1,49 @@
 package entity
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Message struct {
+	ID        string
+	Role      string
+	Content   string
+	Tokens    int
+	Model     *Model
+	CreatedAt time.Time
+}
+
+func NewMessage(role, content string, model *Model) (*Message, error) {
+	msg = &Message{
+		ID:        uuid.New().String(),
+		Role:      role,
+		Content:   content,
+		Model:     model,
+		CreatedAt: time.Now(),
+	}
+
+	if err := msg.validate(); err != Nil {
+		return nil, err
+	}
+
+	return msg, nil
+}
+
+func (m *Message) Validade() error {
+	if m.Role != "user" && m.Role != "system" && m.Role != "assistant" {
+		return errors.New("invalid role")
+	}
+
+	if m.Content == "" {
+		return errors.New("Content is empty")
+	}
+
+	if m.CreatedAt.IsZero() {
+		return errors.New("Invalid created at")
+	}
+
+	return nil
+}
